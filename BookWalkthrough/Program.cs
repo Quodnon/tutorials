d4129d42195a852467d77e057513703dec74db16
quodnon <feanorfeanturi@gmail.com>
2014-08-05 21:33:25 +0300

    massives2

--- BookWalkthrough/Program.cs
+++ BookWalkthrough/Program.cs
@@ -26,10 +26,12 @@ namespace Example
 
         static void Main(string[] args)
         {
-            int[] myArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; for (int i = 0; i < 10; i++)
+            int[] myArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            foreach (int elem in myArray)
             {
 
-                Console.Write("{0} ",myArray[i]);
+                Console.Write("{0} ",elem* elem);
 
             }
             Console.ReadLine();
